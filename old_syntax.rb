dec7381f4db9ce37e372ccfd3a13b4be41324fc8
Piotr Banasik <piotr.banasik@gmail.com>
2015-01-19 13:40:03 -0800

    Adding standard 1.8 style class

---
+++ old_syntax.rb
@@ -0,0 +1,42 @@
+class OldSyntax
+
+  def complex(foo = true)
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+    puts "bla bla"
+
+    if true
+      if false
+        if true
+          if false
+            if true
+              100.times do
+                sleep 1
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+end
